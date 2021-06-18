415a28adcaaa7f42118578510fa159ce6e53e11e
Snipez9492 <waleibk92@gmail.com>
2021-06-18 12:13:23 -0400

    Second Time

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Lets see if this works.")
+import webbrowser
+
+run = True
+
+name = input("State your name: ")
+password = "Waleibk92"
+attempt = 3
+
+while run:
+    if not name:
+        retry = input("Please enter a name: ")
+        name = retry
+        continue
+    else:
+        print("Next to meet you", name)
+
+    access = input("What is the password: ")
+
+    if access == password:
+        print("Access is granted, welcome", name)
+        webbrowser.open('https://www.nba.com/')
+        run = False
+    else:
+        print("Please try again.")
+        attempt = attempt - 1
+
+        if attempt == 0:
+            print("You have exceeded the number of attempt. Goodbye!")
+            break
